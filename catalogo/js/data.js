4ce7e437b74557352a14723395ed58b0b881757b
Danylo Moraes <16506747+DanyloMoraes@users.noreply.github.com>
2026-05-12 23:22:19 -0300

    add more items to the catalog

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -18,24 +18,40 @@ export const categories = [
                 img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
             },
+            {
+                img: "https://i.ytimg.com/vi/KVrNGRie78o/hqdefault.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=MAqm-1zUyno"
+            }
         ]
     },
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://i.ytimg.com/vi/Wb3kaP580kA/maxresdefault.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Wb3kaP580kA" },
+            { img: "https://i.ytimg.com/vi/xvJL6qQ249A/maxresdefault.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=4vi6ZWe_3Yc" },
+            { img: "https://i.ytimg.com/vi/gpv7ayf_tyE/maxresdefault.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=gpv7ayf_tyE" },
+            { img: "https://i.ytimg.com/vi/oO59n_mLyHA/maxresdefault.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=oO59n_mLyHA" },
+            { img: "https://i.ytimg.com/vi/OmCnSsPLREQ/maxresdefault.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=OmCnSsPLREQ" }
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://i.ytimg.com/vi/P_-r11fcDTc/maxresdefault.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=P_-r11fcDTc" },
+            { img: "https://i.ytimg.com/vi/iS5xXr-GOnM/maxresdefault.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=iS5xXr-GOnM" },
+            { img: "https://i.ytimg.com/vi/_H1G9BsxhDw/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=KoD1DaIlUng" },
+            { img: "https://i.ytimg.com/vi/6zp3mt5Dd2c/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=6zp3mt5Dd2c" },
+            { img: "https://i.ytimg.com/vi/WpSweeyuHb8/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=ga0iTWXCGa0" }
+        ]
+    },
+    {
+        title: "Animes",
+        items: [
+            { img: "https://i.ytimg.com/vi/s99s4VCtCP8/maxresdefault.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=rNlKIMdW4x8" },
+            { img: "https://i.ytimg.com/vi/htgcz87-Wqk/maxresdefault.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=htgcz87-Wqk" },
+            { img: "https://i.ytimg.com/vi/ww06yGPM7Kc/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=ww06yGPM7Kc" },
+            { img: "https://i.ytimg.com/vi/nA8KmHC2Z-g/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=nA8KmHC2Z-g" },
+            { img: "https://i.ytimg.com/vi/aADTX6CmIx4/maxresdefault.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=aADTX6CmIx4" }
         ]
     }
 ];
